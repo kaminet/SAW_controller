e92b8251b4c0c66791b3221a5acdffe04f914d59
kaminet <kamilcwik@o2.pl>
2020-05-09 01:27:40 +0200

    Prepare button actions

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,11 @@
 #define myLED 13           // onboard LED
 #define myStartButton 16 // A2 PC2 Button Pin
 #define myRelayPin 17   // A3 PC3 Relay for UVC LAMP
+#define UvcTime 120000UL   // disinfection time
+#define DoorTime 2000UL   // door seafty time
 
 unsigned long curTime = 0UL;   // will store current time to avoid multiple millis() calls
+bool firstLong = false;  // first UvcTime cycle longer
 
 // The actions I ca do...
 typedef enum
@@ -30,6 +33,9 @@ typedef enum {
 LedState;
 
 LedState statusLed = LED_OFF; // Led disabled on startup
+
+// Setup a new OneButton on pin myStartButton.  
+OneButton button(myStartButton, true);
 
 void statusLED() {
   switch (statusLed)
@@ -64,18 +70,84 @@ void statusLED() {
   }
 }
 
+// this function will be called when the button was pressed 1 time and them some time has passed.
+void myClickFunction() {
+  switch (nextAction)
+  {
+    case ACTION_FORCE_WAIT:
+      nextAction = ACTION_FORCE_ON;
+      break;
+    case ACTION_FORCE_ON:
+      nextAction = ACTION_FORCE_WAIT;
+      break;
+    default:
+
+    break;
+  }
+} // myClickFunction
+
+// this function will be called when the button was pressed 2 times in a short timeframe.
+void myDoubleClickFunction() {
+  switch (nextAction)
+  {
+    case ACTION_FORCE_WAIT:
+      nextAction = ACTION_OPEN;
+      break;
+    case ACTION_FORCE_ON:
+      nextAction = ACTION_OPEN;
+      break;
+    case ACTION_OPEN:
+      if (firstLong == true)
+      {
+         firstLong = false;
+         nextAction = ACTION_FORCE_WAIT;
+      } else {
+        firstLong = true;
+      }
+      break;
+    default:
+    // This should never occur
+    break;
+  }
+} // myDoubleClickFunction
+
+// this function will be called when the button was pressed for one second.
+void myLongPressFunction() {
+  switch (nextAction)
+  {
+    case ACTION_OPEN:
+      nextAction = ACTION_IDLE;
+      break;
+    default:
+    
+    break;
+  }
+} // myLongPressFunction
+
 void setup()
 {
   // put your setup code here, to run once:
   pinMode(myLED, OUTPUT);         // sets the digital pin as output
   pinMode(myRelayPin, OUTPUT); // sets the digital pin as output
-}
+  // link the myClickFunction function to be called on a click event.   
+  button.attachClick(myClickFunction);
+
+  // link the doubleclick function to be called on a doubleclick event.   
+  button.attachDoubleClick(myDoubleClickFunction);
+
+  // link the longpress function to be called on a longpress event.   
+  button.attachLongPressStart(myLongPressFunction);
+
+  // set 80 msec. debouncing time. Default is 50 msec.
+  button.setDebounceTicks(80);
+} // setup
 
 void loop()
 {
   // put your main code here, to run repeatedly:
   curTime = millis();
   statusLED();
+  button.tick(); // keep watching the push button:
 
   switch (nextAction)
   {
