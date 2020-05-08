5dfa21a8d3b51360180d781cee4d5440e64b79f7
kaminet <kamilcwik@o2.pl>
2020-05-08 16:12:23 +0200

    Define IO

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
+// ---- PREPARE I/O
+#define LED 13           // onboard LED
+#define myStartButton 16 // A2 PC2 Button Pin
+#define migRelayPin 17   // A3 PC3 Relay for UVC LAMP
+
 void setup() {
   // put your setup code here, to run once:
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
 }
