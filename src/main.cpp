a61bab7fdbf495c31067cf21fcb21c192bf78563
kaminet <kamilcwik@o2.pl>
2020-05-08 16:49:15 +0200

    Test output

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,13 @@
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+    digitalWrite(LED, true);
+    delay(10000);
+    digitalWrite(LED, false);
+    delay(1000);
 }
